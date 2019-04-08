dc434f065765d98d45e188f10ecbb34d4131bc77
Ashok <ashokkafle96@gmail.com>
2019-04-08 00:24:57 -0400

    fixed some API key and method errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'subtracker';
+export class AppComponent implements OnInit {
+  user: firebase.User;
+  
+  constructor(
+    private afAuth: AngularFireAuth,
+    private loginService: LoginService
+  ) {}
+  ngOnInit(){
+    
+  }
 }
+
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,19 @@ import {AngularFireModule} from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
 import { config } from 'rxjs';
+import{ AngularFirestoreModule } from '@angular/fire/firestore';
 import { HttpClient } from 'selenium-webdriver/http';
 
 
-export const firebaseCOnfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCnPQP8EBUxLOrtt9Rd3e0oh1RMKZE0yxc",
   authDomain: "subtracker-a0303.firebaseapp.com",
   databaseURL: "https://subtracker-a0303.firebaseio.com",
   projectId: "subtracker-a0303",
   storageBucket: "subtracker-a0303.appspot.com",
   messagingSenderId: "341711558431"
-}
+};
+
 
 @NgModule({
   declarations: [
@@ -27,9 +29,8 @@ export const firebaseCOnfig = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(config),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
-
     FormsModule
   ],
   providers: [],
